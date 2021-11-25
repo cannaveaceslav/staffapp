36e7941a5b1326a662e7ac93905c6630890cb440
vcanna <vcanna@inthergroup.com>
2021-11-25 20:08:24 +0200

    STAFFAPP - Thymeleaf integrated - @Controller instead of @Restcontroller (it adds @ResponseBody automatically)

--- backend/src/main/java/com/staffapp/backend/controller/UserRegistrationController.java
+++ backend/src/main/java/com/staffapp/backend/controller/UserRegistrationController.java
@@ -2,21 +2,24 @@ package com.staffapp.backend.controller;
 
 import com.staffapp.backend.service.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "/registration")
 @AllArgsConstructor
 public class UserRegistrationController {
 
   private final RegistrationService registrationService;
 
-  @GetMapping
+ /* @GetMapping
   public String register() {
     return "<html>\n" + "<header><title>Welcome</title></header>\n" +
             "<body>\n" + "Register new account\n" +
@@ -31,18 +34,25 @@ public class UserRegistrationController {
             "</body>\n" +
             "</html>";
   }
-
-//  @GetMapping
-//  public String register() {
-//    return "register";
-//  }
+*/
+  @GetMapping
+  public String register() {
+    return "register";
+  }
+  @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name");
+        return "greeting";
+    }
 
   @PostMapping
+  @ResponseBody
   public String register(@RequestBody RegistrationRequest registrationRequest) {
     return registrationService.register(registrationRequest);
   }
 
   @GetMapping(path = "confirm")
+    @ResponseBody
   public String confirm(@RequestParam("token") String token) {
     return registrationService.confirmToken(token);
   }
