2557b32e05f06726c6eb584ca41d263cd23f733e
vcanna <vcanna@inthergroup.com>
2021-11-28 15:52:28 +0200

    STAFFAPP - removed props for Thymeleaf - add Bean for Thymeleaf - changed html

--- backend/src/main/java/com/staffapp/backend/config/StaffAppConfig.java
+++ backend/src/main/java/com/staffapp/backend/config/StaffAppConfig.java
@@ -1,10 +1,15 @@
 package com.staffapp.backend.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
+@EnableAutoConfiguration
 public class StaffAppConfig {
   @Bean
   public CommonsRequestLoggingFilter logFilter() {
@@ -17,4 +22,27 @@ public class StaffAppConfig {
     filter.setAfterMessagePrefix("REQUEST DATA : ");
     return filter;
   }
+      @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCacheable(false);
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.addTemplateResolver(templateResolver());
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
 }
