0a13e01ebaabbf0248cd7469d1e726ea0d49713c
vcanna <vcanna@inthergroup.com>
2021-12-21 17:02:29 +0200

    STAFFAPP FR17_connect_frontend - add angular project 'frontend'; - add item, employee, home, layout components;

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./layout/layout.component";
+import {ItemsComponent} from "./items/items.component";
+import {EmployeesComponent} from "./employees/employees.component";
+import {ReportsComponent} from "./reports/reports.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'layout', component: LayoutComponent},
+  {path: 'employees', component: EmployeesComponent},
+  {path: 'items', component: ItemsComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ItemsComponent } from './items/items.component';
+import { ReportsComponent } from './reports/reports.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    EmployeesComponent,
+    ItemsComponent,
+    ReportsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
