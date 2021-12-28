3970b61ffaf6243997140b44362637b2319fa952
vcanna <vcanna@inthergroup.com>
2021-12-28 21:16:55 +0200

    STAFFAPP FR18_implement_login_v3 - add employee controller - add migration for employees and departments

--- backend/src/main/java/com/staffapp/backend/controller/Employee/EmployeesController.java
+++ backend/src/main/java/com/staffapp/backend/controller/Employee/EmployeesController.java
@@ -1,11 +1,20 @@
 package com.staffapp.backend.controller.Employee;
 
+import com.staffapp.backend.model.Response;
+import com.staffapp.backend.service.employee.EmployeeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @AllArgsConstructor
@@ -13,9 +22,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Api("Controller for all employees page")
 public class EmployeesController {
 
-  @ApiOperation("Show all employees from employee table. Return view 'employees.html'")
+  private final EmployeeService employeeService;
+
+
+  @CrossOrigin(origins = "http://localhost:8080")
   @GetMapping
-  public String showAllEmployees(){
-    return "employees";
+  @ApiOperation("Method returns a response with the map with key=employees and value=list of all employees")
+  public ResponseEntity<Response> getLocations() {
+    return ResponseEntity.ok(
+            Response.builder()
+                    .timeStamp(now())
+                    .data(Collections.singletonMap("employees", employeeService.list(20)))
+                    .message("Employees retrieved")
+                    .status(OK)
+                    .statusCode(OK.value())
+                    .build()
+    );
   }
 }
