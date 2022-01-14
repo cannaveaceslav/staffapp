ca95963a705cb9bb05df99d6a563beb0627e304a
vcanna <vcanna@inthergroup.com>
2022-01-14 11:50:36 +0200

    STAFFAPP FR18_implement_login_v3 - FIXED getPostition

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {LayoutService} from "../service/layout.service";
 import {map, Observable, of, startWith} from "rxjs";
 import {AppState} from "../interface/app-state";
@@ -6,6 +6,7 @@ import {CustomResponse} from "../interface/custom-response";
 import {DataState} from "../enum/data-state.enum";
 import {catchError} from "rxjs/operators";
 import {CdkDragStart, DragAxis} from "@angular/cdk/drag-drop";
+import {Location} from '../interface/location';
 
 @Component({
   selector: 'app-layout',
@@ -15,15 +16,12 @@ import {CdkDragStart, DragAxis} from "@angular/cdk/drag-drop";
 export class LayoutComponent implements OnInit {
   appState$!: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
+  @Input() locationDTO!: Location;
   public dragging!: boolean;
-  // public 	position!: {x: number, y: number}
-
 
 
   constructor(private layoutService: LayoutService) {
   }
-
-
 
 
   ngOnInit(): void {
@@ -39,17 +37,22 @@ export class LayoutComponent implements OnInit {
       );
   }
 
-  onDragEnded(event: { source: { getRootElement: () => any; }; }) {
+  onDragEnded(event: { source: { getRootElement: () => any; }; }, location:Location) {
+
     let element = event.source.getRootElement();
     let boundingClientRect = element.getBoundingClientRect();
     let parentPosition = this.getPosition(element);
     console.log('x: ' + (boundingClientRect.x - parentPosition.left), 'y: ' + (boundingClientRect.y - parentPosition.top));
+    this.locationDTO.pos_x = boundingClientRect.x - parentPosition.left;
+    this.locationDTO.pos_y = boundingClientRect.y - parentPosition.top;
+    console.log(this.locationDTO.pos_y+"   "+this.locationDTO.pos_x);
+    this.saveLocation(this.locationDTO);
 
   }
 
   getPosition(el: any) {
-    let x = 50;
-    let y = 40;
+    let x = 0;
+    let y = 0;
     while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
       x += el.offsetLeft - el.scrollLeft;
       y += el.offsetTop - el.scrollTop;
@@ -58,8 +61,22 @@ export class LayoutComponent implements OnInit {
     return {top: y, left: x};
   }
 
-    public handleDragStart(event: CdkDragStart): void {
+  public handleDragStart(event: CdkDragStart): void {
     this.dragging = true;
+  }
+
+  saveLocation(location: Location): void {
+    this.appState$ = this.layoutService.save$(location)
+      .pipe(
+        map(response => {
+          console.log('saving');
+          return {dataState: DataState.LOADED_STATE, appData: response}
+        }),
+        startWith({dataState: DataState.LOADING_STATE}),
+        catchError((error: string) => {
+          return of({dataState: DataState.ERROR_STATE, error: error})
+        })
+      );
   }
 
   public handleClick(event: MouseEvent): void {
