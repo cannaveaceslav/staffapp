9e9d79076b77c0e7f7933b148ecbf20534b420c7
vcanna <vcanna@inthergroup.com>
2022-02-16 22:28:18 +0200

    FR24_mobile_apk - setted HTTP call to Backend

---
+++ mobile/app/src/main/java/com/staffapp/mobile/RetrofitClient.java
@@ -0,0 +1,71 @@
+package com.staffapp.mobile;
+
+import android.util.Base64;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.staffapp.mobile.adapter.InterfaceAdapter;
+import com.staffapp.mobile.model.CustomResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    //    public static final String BASE_URL = "http://localhost:8080";
+    private static final String AUTH = "Basic " + Base64.encodeToString(("admin:admin").getBytes(), Base64.NO_WRAP);
+    public static final String BASE_URL = "http://192.168.0.5:8080";
+    private static RetrofitClient mInstance;
+    private Retrofit retrofit;
+
+
+    private RetrofitClient() {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(
+                        new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Request original = chain.request();
+
+                                Request.Builder requestBuilder = original.newBuilder()
+                                        .addHeader("Authorization", AUTH)
+                                        .method(original.method(), original.body());
+                                Request request = requestBuilder.build();
+                                return chain.proceed(request);
+
+                            }
+                        }
+                ).build();
+
+
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (mInstance == null) {
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+
+
+}
---
+++ mobile/app/src/main/java/com/staffapp/mobile/adapter/InterfaceAdapter.java
@@ -0,0 +1,48 @@
+package com.staffapp.mobile.adapter;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+public class InterfaceAdapter implements JsonSerializer, JsonDeserializer<Object> {
+
+    private static final String CLASSNAME = "CLASSNAME";
+    private static final String DATA = "DATA";
+
+    public Object deserialize(JsonElement jsonElement, Type type,
+                              JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        JsonPrimitive prim = (JsonPrimitive) jsonObject.get(CLASSNAME);
+        String className = prim.getAsString();
+        Class klass = getObjectClass(className);
+        return jsonDeserializationContext.deserialize(jsonObject.get(DATA), klass);
+    }
+
+    public JsonElement serialize(Object jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty(CLASSNAME, jsonElement.getClass().getName());
+        jsonObject.add(DATA, jsonSerializationContext.serialize(jsonElement));
+        return jsonObject;
+    }
+
+    /****** Helper method to get the className of the object to be deserialized *****/
+    public Class getObjectClass(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+            throw new JsonParseException(e.getMessage());
+        }
+    }
+
+
+}
+
